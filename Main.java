ccd7024ba8e5216e06964e8082cee2a0571342b6
Sampathcode12 <lsampathlk4@gmail.com>
2024-12-30 22:21:33 +0530

    second loop

---
+++ Main.java
@@ -0,0 +1,31 @@
+public class Main {
+    public static void main(String[] args) {
+        int rows = 5; // Number of rows (half of the diamond)
+
+        // Upper half of the diamond
+        for (int i = 1; i <= rows; i++) {
+            // Print spaces
+            for (int j = i; j < rows; j++) {
+                System.out.print(" ");
+            }
+            // Print stars
+            for (int j = 1; j <= (2 * i - 1); j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        // Lower half of the diamond
+        for (int i = rows - 1; i >= 1; i--) {
+            // Print spaces
+            for (int j = rows; j > i; j--) {
+                System.out.print(" ");
+            }
+            // Print stars
+            for (int j = 1; j <= (2 * i - 1); j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
